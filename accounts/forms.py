5ded3ed2fea9a5f660fff4f8f1a6dfbb1757d0d3
teclados <wjdtmdwo07@daum.net>
2020-01-28 01:32:02 +0900

    09로그인 아이디로서 이메일 사용하기

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,27 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import validate_email
 
 
 class SignupForm(UserCreationForm):
     pass
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].validators = [validate_email]
+        self.fields['username'].help_text = 'Enter Email Format.'
+        self.fields['username'].label = 'Email'
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = user.username
+        if commit:
+            user.save()
+        return user
+
+    '''
+    def clean_username(self):
+        value = self.cleaned_data.get('username')
+        if value:
+            validate_email(value)
+        return value
+    '''
