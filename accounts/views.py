29eb3e823d55395cd5cdd0e5bbc9ea5ec750aeb9
teclados <wjdtmdwo07@daum.net>
2020-01-28 01:19:42 +0900

    08회원가입 시에 next인자 처리하기

--- accounts/views.py
+++ accounts/views.py
@@ -20,7 +20,8 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             auth_login(request, user)  # 바로 로그인 처리
-            return redirect('profile')
+            next_url = request.GET.get('next') or 'profile'
+            return redirect(next_url)
     else:
         form = SignupForm()
     return render(request, 'accounts/signup.html', {
@@ -43,7 +44,8 @@ class SignupView(CreateView):
     template_name = 'accounts/signup.html'
 
     def get_success_url(self):
-        return resolve_url('profile')
+        next_url = self.request.GET.get('next') or 'profile'
+        return resolve_url(next_url)
 
     def form_valid(self, form):
         user = form.save()
