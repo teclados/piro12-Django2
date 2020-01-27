5f9a4c39a99d59cc32bd186980db41e785d06015
teclados <wjdtmdwo07@daum.net>
2020-01-28 01:08:21 +0900

    07회원가입과 동시에 로그인하기

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, resolve_url
 from django.views.generic import CreateView
+from django.contrib.auth import login as auth_login
 
 from accounts.forms import SignupForm
 
@@ -18,7 +19,8 @@ def signup(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
-            return redirect(settings.LOGIN_URL)
+            auth_login(request, user)  # 바로 로그인 처리
+            return redirect('profile')
     else:
         form = SignupForm()
     return render(request, 'accounts/signup.html', {
@@ -26,7 +28,27 @@ def signup(request):
     })
 '''
 
+
+'''CBV(바로 로그인 안하고 그냥 회원가입만)
 signup = CreateView.as_view(model=User,
                             form_class=SignupForm,
                             success_url=settings.LOGIN_URL,
                             template_name='accounts/signup.html')
+'''
+
+
+class SignupView(CreateView):
+    model = User
+    form_class = SignupForm
+    template_name = 'accounts/signup.html'
+
+    def get_success_url(self):
+        return resolve_url('profile')
+
+    def form_valid(self, form):
+        user = form.save()
+        auth_login(self.request, user)
+        return redirect(self.get_success_url())
+
+
+signup = SignupView.as_view()
